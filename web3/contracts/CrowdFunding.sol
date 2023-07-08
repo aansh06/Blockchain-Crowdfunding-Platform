c752ec2c58480fd9c8248d9fb8a25213c774f1b6
aansh06 <aansh0611@gmail.com>
2023-07-08 15:07:56 +0530

    client folder

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -10,31 +10,19 @@ contract CrowdFunding {
         uint256 deadline;
         uint256 amountCollected;
         string image;
-        address[] donator;
-        uint256[] donation;
+        address[] donators;
+        uint256[] donations;
     }
-    // mapping the structure campaign
+
     mapping(uint256 => Campaign) public campaigns;
 
-    uint256 public numberofCampaigns = 0; // public variable
+    uint256 public numberOfCampaigns = 0;
 
-    // To initailize a new Campaign
-    function createCampaign(
-        address _owner,
-        string memory _title,
-        string memory _description,
-        uint256 _target,
-        uint256 _deadline,
-        string memory _image
-    ) public returns (uint256) {
-        Campaign storage campaign = campaigns[numberofCampaigns];
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        // checking if the deadline is valid
-        require(
-            campaign.deadline < block.timestamp,
-            "Deadline should be in future!"
-        );
-        // only run if the deadline is in future
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
         campaign.owner = _owner;
         campaign.title = _title;
         campaign.description = _description;
@@ -43,48 +31,39 @@ contract CrowdFunding {
         campaign.amountCollected = 0;
         campaign.image = _image;
 
-        //now inc no of campaigns
-        numberofCampaigns++;
+        numberOfCampaigns++;
 
-        // index of the created campaign
-        return numberofCampaigns - 1;
+        return numberOfCampaigns - 1;
     }
 
-    //to donate amount to a particular campaign
     function donateToCampaign(uint256 _id) public payable {
-        uint256 amount = msg.value; // amount recieved
+        uint256 amount = msg.value;
 
         Campaign storage campaign = campaigns[_id];
 
-        campaign.donator.push(msg.sender); // storing donator address
-        campaign.donation.push(amount); // storing donated amount
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
 
-        // wether amount is sent
-        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
 
-        // inc amount collected
-        if (sent) {
+        if(sent) {
             campaign.amountCollected = campaign.amountCollected + amount;
         }
     }
 
-    // to get a list of all the listed campaign
-    function getCampaign() public view returns (Campaign[] memory) {
-        Campaign[] memory allCampaigns = new Campaign[](numberofCampaigns);
-
-        for (uint256 i = 0; i < numberofCampaigns; i++) {
-            Campaign storage item = campaigns[i];
-            allCampaigns[i] = item;
-        }
-        return allCampaigns;
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    // to get a list of all the donators
-    function getDonators(uint256 _id)
-        public
-        view
-        returns (address[] memory, uint256[] memory)
-    {
-        return (campaigns[_id].donator, campaigns[_id].donation);
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
     }
 }
